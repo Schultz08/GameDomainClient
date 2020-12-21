09325b1bde85fdd70485925bbe2071dacebf1110
Jess Schultz <schultz.jess.a@outlook.com>
2020-12-21 10:33:00 -0500

    Setup base client, added Auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Auth} from "./Components/index"
+import { Route, BrowserRouter as Router } from "react-router-dom"
 
-function App() {
+
+
+
+const App: React.FC = () => {
+
+  // let token = localStorage.getitem("sessionToken")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Router>
+            <Route path="/login" component={Auth} />
+            <Route path="/logout" component={Auth} />
+          </Router>
     </div>
   );
 }
---
+++ src/Components/Auth/Auth.tsx
@@ -0,0 +1,142 @@
+import React from "react"
+import "./Auth.css"
+import { Button } from '@material-ui/core'
+
+
+
+class Auth extends React.Component {
+    state = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        userName: "",
+        password: "",
+        userRole: "",
+        login: true,
+    }
+
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+
+        let url = this.state.login ? "http://localhost:3000/user/login" : "http://localhost:3000/user/register"
+
+        const body = this.state.login ? {
+            email: this.state.email,
+            password: this.state.password
+        } : {
+                email: this.state.email,
+                password: this.state.password,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                userName: this.state.userName
+            }
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json())
+            .then(data => {
+                localStorage.setItem("sessionToken", data.token)
+            })
+    }
+
+    handleLogout = () => {
+            localStorage.setItem("sessionToken", "")
+    }
+
+    title = () => {
+        return this.state.login ? "Login" : "Signup";
+    }
+
+    logginToggle = (event: any) => {
+        event.preventDefault();
+        this.setState({ login: !this.state.login })
+    }
+
+    signupFields = () => {
+        return !this.state.login ?
+            <div>
+                <input
+                    type="text"
+                    value={this.state.firstName}
+                    onChange={(event) => this.setState({ firstName: event.target.value })}
+                    name="firstName"
+                ></input>
+                <input
+                    type="text"
+                    value={this.state.lastName}
+                    onChange={(event) => this.setState({ lastName: event.target.value })}
+                    name="lastName"
+                ></input>
+                <input
+                    type="text"
+                    value={this.state.userName}
+                    onChange={(event) => this.setState({ userName: event.target.value })}
+                    name="userName"
+                ></input>
+            </div>
+            :
+            null
+    }
+
+    loginButtons = () => {
+        return this.state.login ?
+            <div>
+                <Button color="primary"
+                    className="subButton"
+                    type="submit"
+                >Login!</Button>
+                <Button color="primary"
+                    onClick={(e) => this.logginToggle(e)}
+                    className="subButton"
+                >Sign Up!</Button>
+            </div> 
+            :
+            <div>
+                <Button color="primary"
+                    className="subButton"
+                    type="submit"
+                >Register!</Button>
+                <Button color="primary"
+                    onClick={(e) => this.logginToggle(e)}
+                    className="subButton"
+                >Login!</Button>
+            </div>
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <div className="innerCard">
+                    <form onSubmit={(e) => this.handleSubmit(e)}>
+                        <h2>{this.title()}</h2>
+                        {this.signupFields()}
+                        <input
+                            type="email"
+                            value={this.state.email}
+                            onChange={(event) => this.setState({ email: event.target.value })}
+                            name="email"
+                            placeholder="Email@email.com"
+                        ></input>
+                        <input
+                            type="password"
+                            value={this.state.password}
+                            onChange={(event) => this.setState({ password: event.target.value })}
+                            name="password"
+                            placeholder="password"
+                        ></input>
+                        <br />
+                        {this.loginButtons()}
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Auth;
